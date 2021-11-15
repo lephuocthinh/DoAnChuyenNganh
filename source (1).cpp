978923983b8c4662d7454e62a71071d2cb66e1c1
lephuocthinh <93901093+lephuocthinh@users.noreply.github.com>
2021-11-15 17:21:05 +0700

    Add files via upload

---
+++ source (1).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int tong(int a, int b);
+int main()
+{
+	int a, b;
+	char phepToan;
+	cout << "Nhap so nguyen 1: ";
+	cin >> a;
+	cout << "Nhap so nguyen 2: ";
+	cin >> b;
+	tong(a,b);
+	
+	system("pause");
+	return 0;
+}
+int tong(int a, int b){
+	return a+b;
+}
